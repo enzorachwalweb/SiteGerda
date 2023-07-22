100e9ad504fa83e1beb53907dc8b1c5fe47a34fa
enzorachwalweb <119449302+enzorachwalweb@users.noreply.github.com>
2023-07-22 17:17:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// Define a pasta de publicação como "public"
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Números iniciais na tabela
 let numero1 = 100;
